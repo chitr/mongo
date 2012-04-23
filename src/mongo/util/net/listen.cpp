c83a50f19d3ea24c1d80c6d7d9b7812acc47ec00
Santosh <santosh@8kpc.com>
2012-04-23 12:50:05 +0530

    removing commented code

--- src/mongo/util/net/listen.cpp
+++ src/mongo/util/net/listen.cpp
@@ -250,7 +250,6 @@ namespace mongo {
 
             maxSelectTime.tv_sec = 0;
             maxSelectTime.tv_usec = 10000;
-//            const int ret = select(maxfd+1, fds, NULL, NULL, &maxSelectTime);
             const int ret = select(maxfd+1, fds, NULL, NULL, NULL);
 
             if (ret == 0) {
